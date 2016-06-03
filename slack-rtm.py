b6f0ed5979e6cfc0fe2a9a031a45935005f3c625
Chris Jack <cjack@csag.uct.ac.za>
2016-06-03 10:43:03 +0200

    Slack RTM using websocket-client module

--- slack-rtm.py
+++ slack-rtm.py
@@ -2,14 +2,32 @@ import slack
 import sys
 import json
 import tornado.websocket
+import websocket
 
 config = json.loads(open(sys.argv[1]).read())
 
 
 s = slack.Slack(config['slack']['token'])
 
+im = s.open_im('U04QRTZ3X')
+channel = im['channel']['id']
+
 response = s.rtm_start()
 
 ws_url = response['url']
 
+ws = websocket.create_connection(ws_url)
 
+id = 1
+while True:
+	message_string = ws.recv()
+	if message_string != "":
+		
+		message = json.loads(message_string)
+		print message
+
+		if "type" in message and message["type"] == "message":
+			response = {"id":id, "channel":channel, "type":"message", "text":"You said: {}".format(message["text"])}
+			print response
+			ws.send(json.dumps(response))
+			id += 1
